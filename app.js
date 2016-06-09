43d1806836e811c86eb70cedf420f7c414867699
John Conley <JPConley01@users.noreply.github.com>
2016-06-09 11:33:24 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,77 @@
+angular.module("movieList", [])
+    .controller("movieController", movieController);
+
+
+function movieController() {
+    var movie = this;
+
+    // TODO: implement API
+
+    movie.initialize = function() {
+        console.log(localStorage);
+        movie.items = JSON.parse(localStorage.getItem('items'));
+// TODO: if localStorage.getItems returns undefined DO NOT parse
+        if (!movie.items) {
+            localStorage.removeItem('items');
+
+            movie.items = [{
+                "name": "Terminator 2",
+                "digital": true,
+                "physical": false
+            }, {
+                "name": "Speed",
+                "digital": false,
+                "physical": true
+            }, {
+                "name": "The Matrix",
+                "digital": true,
+                "physical": true
+            }];
+
+            movie.save();
+
+        }
+        console.log(movie.items);
+    };
+
+    movie.toggleEditMode= function(item){
+      item.editMode = !item.editMode;
+      if (!item.editMode){
+        movie.save();
+        console.log("Movie Saved!");
+      }
+
+    };
+
+    movie.itemCreate = function() {
+        return {
+            "name": "",
+            "digital": false,
+            "physical": false
+        };
+    };
+
+
+
+    movie.save = function() {
+        localStorage.removeItem('items');
+        localStorage.setItem('items', JSON.stringify(movie.items));
+    };
+
+    movie.addItem = function() {
+        console.log("Adding movie!", movie.newItem, movie.items);
+        movie.items.push(movie.newItem);
+        console.log(movie.items);
+        movie.newItem = movie.itemCreate();
+
+        movie.save();
+    };
+    movie.newItem = movie.itemCreate();
+
+    movie.deleteItem = function($index) {
+        console.log("Removing movie!");
+        movie.items.splice($index, 1);
+        movie.save();
+    };
+
+}
