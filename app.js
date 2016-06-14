15676cd6878c6339fc32bd22eb2a02b43905885c
John Conley <JPConley01@users.noreply.github.com>
2016-06-14 11:30:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 angular.module("movieList", [])
-    .controller("movieController", movieController);
+    .controller("movieController", ["$http",movieController]);
 
 
-function movieController() {
+function movieController($http) {
     var movie = this;
 
     // TODO: implement API
@@ -10,46 +10,136 @@ function movieController() {
     movie.initialize = function() {
         console.log(localStorage);
         movie.items = JSON.parse(localStorage.getItem('items'));
-// TODO: if localStorage.getItems returns undefined DO NOT parse
+        // TODO: if localStorage.getItems returns undefined DO NOT parse
         if (!movie.items) {
             localStorage.removeItem('items');
 
             movie.items = [{
-                "title": "Terminator 2",
+                "Title": "Terminator 2: Judgment Day",
+                "Year": "1991",
+                "Rated": "R",
+                "Released": "03 Jul 1991",
+                "Runtime": "137 min",
+                "Genre": "Action, Sci-Fi",
+                "Director": "James Cameron",
+                "Writer": "James Cameron, William Wisher Jr.",
+                "Actors": "Arnold Schwarzenegger, Linda Hamilton, Edward Furlong, Robert Patrick",
+                "Plot": "A cyborg, identical to the one who failed to kill Sarah Connor, must now protect her young son, John Connor, from a more advanced cyborg, made out of liquid metal.",
+                "Language": "English, Spanish",
+                "Country": "USA, France",
+                "Awards": "Won 4 Oscars. Another 20 wins & 22 nominations.",
+                "Poster": "http://ia.media-imdb.com/images/M/MV5BMTI4MDAwMDY3N15BMl5BanBnXkFtZTcwODIwMzMzMQ@@._V1._CR46,1,342,473_SY132_CR3,0,89,132_AL_.jpg_V1_SX300.jpg",
+                "Metascore": "75",
+                "imdbRating": "8.5",
+                "imdbVotes": "734,005",
+                "imdbID": "tt0103064",
+                "Type": "movie",
                 "digital": true,
                 "physical": false
             }, {
-                "title": "Speed",
-                "digital": false,
-                "physical": true
-            }, {
-                "title": "The Matrix",
+                "Title": "Speed",
+                "Year": "1994",
+                "Rated": "R",
+                "Released": "10 Jun 1994",
+                "Runtime": "116 min",
+                "Genre": "Action, Adventure, Crime",
+                "Director": "Jan de Bont",
+                "Writer": "Graham Yost",
+                "Actors": "Keanu Reeves, Dennis Hopper, Sandra Bullock, Joe Morton",
+                "Plot": "A young cop must prevent a bomb exploding aboard a city bus by keeping its speed above 50 mph.",
+                "Language": "English",
+                "Country": "USA",
+                "Awards": "Won 2 Oscars. Another 14 wins & 16 nominations.",
+                "Poster": "http://ia.media-imdb.com/images/M/MV5BNzczNDQyMTc2MF5BMl5BanBnXkFtZTgwNjI2NzQxMTE@._V1_SX300.jpg",
+                "Metascore": "78",
+                "imdbRating": "7.2",
+                "imdbVotes": "258,050",
+                "imdbID": "tt0111257",
+                "Type": "movie",
                 "digital": true,
-                "physical": true
+                "physical": false
+
+            }, {
+                "Title": "The Matrix",
+                "Year": "1999",
+                "Rated": "R",
+                "Released": "31 Mar 1999",
+                "Runtime": "136 min",
+                "Genre": "Action, Sci-Fi",
+                "Director": "Lana Wachowski, Lilly Wachowski",
+                "Writer": "Lilly Wachowski, Lana Wachowski",
+                "Actors": "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving",
+                "Plot": "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+                "Language": "English",
+                "Country": "USA",
+                "Awards": "Won 4 Oscars. Another 33 wins & 44 nominations.",
+                "Poster": "http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX300.jpg",
+                "Metascore": "73",
+                "imdbRating": "8.7",
+                "imdbVotes": "1,195,371",
+                "imdbID": "tt0133093",
+                "Type": "movie",
+                "digital": true,
+                "physical": false
+
             }];
 
             movie.save();
 
         }
-        console.log(movie.items);
-    };
 
-    movie.toggleEditMode= function(item){
-      item.editMode = !item.editMode;
-      if (!item.editMode){
-        movie.save();
-        console.log("Movie Saved!");
-      }
+        movie.itemCreate = function(data) {
+            return {
+              "Title": "",
+              "Year": "",
+              "Rated": "",
+              "Released": "",
+              "Runtime": "",
+              "Genre": "",
+              "Director": "",
+              "Writer": "",
+              "Actors": "",
+              "Plot": "",
+              "Language": "",
+              "Country": "",
+              "Awards": "",
+              "Poster": "",
+              "Metascore": "",
+              "imdbRating": "",
+              "imdbVotes": "",
+              "imdbID": "",
+              "Type": "",
+              "digital": true,
+              "physical": false
+            };
+        };
 
-    };
+        movie.getData = function() {
+          console.log("in getData");
+            var title = document.getElementById('newMovieTitle').value.replace(' ', '+');
+            console.log(title);
+            $http({
+                method: "GET",
+                url: "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json"
+            }).then(function(response) {
+                movie.response = response.data;
+                movie.itemCreate(movie.response);
+                console.log("Getting Data", movie.response);
 
-    movie.itemCreate = function() {
-        return {
-            "title": "",
-            "digital": false,
-            "physical": false
+            });
         };
     };
+
+    movie.toggleEditMode = function(item) {
+        item.editMode = !item.editMode;
+        if (!item.editMode) {
+            movie.save();
+            console.log("Movie Saved!");
+        }
+
+    };
+
+
 
 
 
@@ -58,15 +148,16 @@ function movieController() {
         localStorage.setItem('items', JSON.stringify(movie.items));
     };
 
-    movie.addItem = function() {
+    movie.addItem = function(movie) {
         console.log("Adding movie!", movie.newItem, movie.items);
         movie.items.push(movie.newItem);
         console.log(movie.items);
         movie.newItem = movie.itemCreate();
 
         movie.save();
+        movie.newItem = movie.itemCreate();
     };
-    movie.newItem = movie.itemCreate();
+
 
     movie.deleteItem = function($index) {
         console.log("Removing movie!");
