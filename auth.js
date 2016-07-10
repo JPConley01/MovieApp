eee5ec8c5c536bd4328e1dbd7d903c2ccd89dcff
John Conley <JohnPaul@Johns-MacBook-Pro.local>
2016-07-10 14:02:32 -0600

    send data through express

--- auth.js
+++ auth.js
@@ -214,7 +214,8 @@ app.post('/user', app.isAuthenticated, function(req, res) {
 app.get('/user', app.isAuthenticated, function(req, res) {
   console.log(req.params.name);
   User.findOne({username: req.params.name}, function(err, user) {
-    return user;
+    console.log(user);
+    res.send(user);
   });
 });
 
